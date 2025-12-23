c6f5a621e2290eb94ff0858bbc7fd58a644ff335
yoshimu-urami-nats <yohanenokubi@gmail.com>
2025-12-24 02:28:09 +0900

    PostgreSQLのための環境設定

--- kakeibo_app/settings.py
+++ kakeibo_app/settings.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 """
 Django settings for kakeibo_app project.
 
@@ -77,8 +80,18 @@ WSGI_APPLICATION = 'kakeibo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            'django.db.backends.sqlite3'
+        ),
+        'NAME': os.getenv(
+            'DB_NAME',
+            BASE_DIR / 'db.sqlite3'
+        ),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
     }
 }
 
