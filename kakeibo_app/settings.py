a8031b02d98a453d01fe2ae71adb71163e37c2b8
yoshimu-urami-nats <yohanenokubi@gmail.com>
2026-01-03 03:57:49 +0900

    ローカルDBも本番ポスグレ使用

--- kakeibo_app/settings.py
+++ kakeibo_app/settings.py
@@ -77,44 +77,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kakeibo_app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv(
-#             'DB_ENGINE',
-#             'django.db.backends.sqlite3'
-#         ),
-#         'NAME': os.getenv(
-#             'DB_NAME',
-#             BASE_DIR / 'db.sqlite3'
-#         ),
-#         'USER': os.getenv('DB_USER', ''),
-#         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', ''),
-#         'PORT': os.getenv('DB_PORT', ''),
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'kakeibo_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Yoshimura',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASE_URL = os.environ["DATABASE_URL"]  # ← 必須（無いと即エラー）
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600),
 }
+
 
 
 # Password validation
