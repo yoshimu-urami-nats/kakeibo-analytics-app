d26041195fc1ca404bcc406c64740377f3bf46f7
yoshimu-urami-nats <yohanenokubi@gmail.com>
2026-02-04 16:02:37 +0900

    サマリ調整

--- transactions/views.py
+++ transactions/views.py
@@ -349,6 +349,8 @@ def transaction_list(request):
             + summary["wrx_per_person"]
         )
 
+        summary["fixed_total"] = summary["rent"] + summary["rent_renewal"] + summary["wrx"]
+
         # 振込額
         summary["transfer_n"] = summary["n_total"] + summary["per_person_total"]
         summary["transfer_y"] = summary["y_total"] + summary["per_person_total"]
